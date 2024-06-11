f924164ae78a248297537ee91ccf812f8f7c23db
AlexVasechkin <vasechkin.alex.v@gmail.com>
2024-06-11 17:10:39 +0300

    Удаление секрета

--- secrets.sh
+++ secrets.sh
@@ -4,3 +4,5 @@ kubectl create secret generic pg-secret --from-literal PASSWORD=my_pass
 kubectl describe secrets
 
 kubectl get secret pg-secret --template={{.data.PASSWORD}} | base64 -d
+
+kubectl delete secrets pg-secret
