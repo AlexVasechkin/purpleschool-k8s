f8def9494e9d68012e3f5a6d297946b93d4f7a2d
AlexVasechkin <vasechkin.alex.v@gmail.com>
2024-06-11 17:09:11 +0300

    Получение значения секрета

---
+++ secrets.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+kubectl create secret generic pg-secret --from-literal PASSWORD=my_pass
+
+kubectl describe secrets
+
+kubectl get secret pg-secret --template={{.data.PASSWORD}} | base64 -d
